f8cce6a28c9afdec99135fa7ca3c8cce04abe6a8
Robert Kristof Paulsen <robert.paulsen@uib.no>
2024-11-04 13:33:57 +0100

    create functionality for loading local JSON

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-console.log("Hello World");
+//import data from "../json/data/data.json" assert { type: "json" };
+//console.log(data);
+
+$(document).ready(async function() {
+    let myData;
+    await fetch("./json/data/data.json")
+        .then((response) => response.json())
+        .then((data) => {
+            //console.log(data);
+            myData = data;
+        })
+        .catch((error) => console.error("Error loading JSON file", error));
+
+    console.log(myData);
+    $('#content').text("Hallo Welt");
+});
