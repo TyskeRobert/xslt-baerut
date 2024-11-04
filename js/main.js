0cf99e59f0a702cd97884b6134576a700eae574a
Robert Kristof Paulsen <robert.paulsen@uib.no>
2024-11-04 13:59:07 +0100

    add favicon

--- js/main.js
+++ js/main.js
@@ -1,16 +1,11 @@
-//import data from "../json/data/data.json" assert { type: "json" };
-//console.log(data);
-
 $(document).ready(async function() {
-    let myData;
+    let data;
     await fetch("./json/data/data.json")
         .then((response) => response.json())
-        .then((data) => {
-            //console.log(data);
-            myData = data;
+        .then((d) => {
+            data = d;
         })
         .catch((error) => console.error("Error loading JSON file", error));
 
-    console.log(myData);
-    $('#content').text("Hallo Welt");
+    $('#content').text(data.text);
 });
