53a55725b2c39e59d194a3f3431239b3f4d7b02e
Robert Kristof Paulsen <robert.paulsen@uib.no>
2024-11-17 11:37:14 +0100

    start html/js work

---
+++ js/loader.js
@@ -0,0 +1,25 @@
+export async function loadCorpus() {
+    let msList;
+    await fetch("./json/data/corpus.json")
+        .then((response) => response.json())
+        .then((data) => {
+            msList = data;
+        })
+        .catch((error) => console.error("Error loading source list: ", error));
+        
+    const result = {
+        date: msList.date,
+        sources: []
+    };
+
+    for (const ms of msList.sources) {
+        await fetch(`./json/data/corpus/${ms}.json`)
+            .then((response) => response.json())
+            .then((data) => {
+                result.sources.push(data)
+            })
+            .catch((error) => console.error(`Error loading source file ${ms}.json:`, error));
+    }
+
+    return result;
+}
--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 $(document).ready(async function() {
-    let data;
-    await fetch("./json/data/data.json")
+    const corpus = loadCorpus();
+    console.log(corpus);
+    
+    const list = document.createElement("ul");
+    for (const ms of corpus.sources) {
+        const item = document.createElement("li");
+        item.textContent = ms;
+        list.append(item);
+    }
+    
+    $('#page').append(list);
+});
+
+async function loadCorpus() {
+    let result;
+    await fetch("./json/data/corpus.json")
         .then((response) => response.json())
         .then((d) => {
-            data = d;
+            result = d;
         })
         .catch((error) => console.error("Error loading JSON file", error));
-
-    $('#content').text(data.text);
-});
+    return result;
+}
